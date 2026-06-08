3cb38e317b382940d68b8ea27c84a1cb1cc6c31d
Cristian Patiño <cristian.pl1996@gmail.com>
2026-06-08 13:54:52 -0500

    changes 34

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig(({ mode }) => ({
     dedupe: ["react", "react-dom", "react/jsx-runtime"],
   },
   optimizeDeps: {
-    include: ["@tanstack/react-query"],
+    include: ["@tanstack/react-query", "leaflet", "react-leaflet"],
   },
   build: {
     rollupOptions: {
